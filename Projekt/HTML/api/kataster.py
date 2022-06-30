8f06aa6b75c634bdf68c00ee05188bc65aa732bb
Donald431 <103324544+Donald431@users.noreply.github.com>
2022-06-30 11:53:22 +0200

    Add files via upload

--- Projekt/HTML/api/kataster.py
+++ Projekt/HTML/api/kataster.py
@@ -6,9 +6,22 @@ import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
 
-db_login = f"host={os.environ.get('PG_HOST')} port={os.environ.get('PG_PORT')} dbname={os.environ.get('PG_DATABASE')} user={os.environ.get('PG_USSER')} password={os.environ.get('PG_PASSWORD')}"
+# pip install -U Flask
+# pip install -U Flask-Cors
+# pip install -U psycopg2
+# pip install -U python-dotenv
 
+# https://de.wikipedia.org/wiki/Cross-Origin_Resource_Sharing
+
+# http://localhost:8085/tree/100005870
+
+load_dotenv()  # Erforderlich zum Laden der zuvor definierten Umgebungsvariablen.
+
+db_login = f"""host={os.environ.get('PG_HOST')} port={os.environ.get('PG_PORT')} 
+               dbname={os.environ.get('PG_DATABASE')} user={os.environ.get('PG_USER')} 
+               password={os.environ.get('PG_PASSWORD')}"""
 db_schema = os.environ.get('PG_SCHEMA')
+print(f" * DB-Schema: {db_schema}")
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,55 +31,106 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return """<!DOCTYPE html><html>
-            <head>
-                <title>Straßenbaumkataster</title>
-            </head>
-            <body>
-                <h1>RESTFUL API Straßenbaumkataster</h1>
-            </body></html>"""
+    return """<!DOCTYPE html><html><title>Straßenbaumkataster</title></head>
+            <body><h1>RESTful API Straßenbaumkataster</h1></body></html>"""
 
 @app.route('/tree/<id>')
 def get_tree(id):
     try:
         with psycopg2.connect(db_login) as connection:
             cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-            cursor.execute(f"""SELECT baumid, 
-                                        baumnummer, 
-                                        sorte_nr, 
-                                        pflanzjahr, 
-                                        kronendurchmesser,
-                                        stammumfang,
-                                        strasse,
-                                        hausnummer,
-                                        ortsteil_nr,
-                                        public.ST_AsGeoJSON(public.ST_Transorm(geom, 4326)) as geojson
-                                FROM {db_schema}.strassenbaumkataster WHERE baumid={id};""")
-            row = cursor.fetchone
+            SQL = f"""SELECT baumid, baumnummer, sorte_nr, pflanzjahr,
+                             kronendurchmesser, stammumfang,
+                             strasse, hausnummer, ortsteil_nr,
+                             public.ST_AsGeoJSON(public.ST_Transform(geom, 4326)) as geojson
+                      FROM {db_schema}.strassenbaumkataster WHERE baumid={id};"""
+            print(SQL)
+            cursor.execute(SQL)
+            row = cursor.fetchone()
             tree = {
-                'type': 'FeutureCollection',
-                'feutures': [
+                'type': 'FeatureCollection',
+                'features': [
                     {
-                        'type': 'Feuture',
-                        'if': row['baumid'],
+                        'type': 'Feature',
+                        'id': row['baumid'],
                         'properties': {
                             'baum_nr': row['baumnummer'],
                             'sorte_nr': row['sorte_nr'],
                             'pflanzjahr': row['pflanzjahr'],
                             'krone': row['kronendurchmesser'],
+                            'stamm': row['stammumfang'],
                             'strasse': row['strasse'],
                             'haus_nr': row['hausnummer'],
-                            'ortsteil_nr': row['ortsteil_nr'],
+                            'ortsteil_nr': row['ortsteil_nr']
                         },
-                        'geometry': json.loads('geojson')
+                        'geometry': json.loads(row['geojson'])
                     }
                 ]
             }
             return tree
     except psycopg2.DatabaseError as error:
-        return {'message': error }
+        return { 'message': error }
     except Exception as error:
-        return {'message': error }
+        return { 'message': error }
+
+@app.route('/trees')
+def get_trees():
+    try:
+        with psycopg2.connect(db_login) as connection:
+            cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            SQL = f"""SELECT baumid, baumnummer, sorte_nr, pflanzjahr,
+                             kronendurchmesser, stammumfang,
+                             strasse, hausnummer, ortsteil_nr,
+                             public.ST_AsGeoJSON(public.ST_Transform(geom, 4326)) as geojson
+                      FROM {db_schema}.strassenbaumkataster LIMIT 5;"""
+            #print(SQL)
+            cursor.execute(SQL)
+            rows = cursor.fetchall()
+
+            feature_collection = {
+                'type': 'FeatureCollection'
+            }
+            features = list()
+            for row in rows:
+                feature = {
+                    'type': 'Feature',
+                        'id': row['baumid'],
+                        'properties': {
+                            'baum_nr': row['baumnummer'],
+                            'sorte_nr': row['sorte_nr'],
+                            'pflanzjahr': row['pflanzjahr'],
+                            'krone': row['kronendurchmesser'],
+                            'stamm': row['stammumfang'],
+                            'strasse': row['strasse'],
+                            'haus_nr': row['hausnummer'],
+                            'ortsteil_nr': row['ortsteil_nr']
+                        },
+                        'geometry': json.loads(row['geojson'])
+                }
+                features.append(feature)
+            feature_collection.update({
+                'features': features
+            })
+            return jsonify(feature_collection)
+    except psycopg2.DatabaseError as error:
+        return { 'message': error }
+    except Exception as error:
+        return { 'message': error }
+
+@app.route('/create', methods=['POST'])
+def create_tree():
+    content = request.get_json()
+    print(content)   
+    try:
+        with psycopg2.connect(db_login) as connection:
+            cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            #cursor.execute("INSERT INTO straßenbaumkataster VALUES(:id, :baum_nr, :sorte_nr, :pflanzjahr, :krone, :stamm, :strasse, :haus_nr, :ortsteil_nr, );", content)
+            return {'message': 'Datensatz eingefügt'}
+    except psycopg2.DatabaseError as error:
+        return { 'message': error }
+    except Exception as error:
+        return { 'message': error }
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('API_PORT'))
